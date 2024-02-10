9c33e80af3592e8f2b457f291e57272a819f39fe
Pranav Raju <pranavraju2020@gmail.com>
2024-02-10 23:56:52 +0530

    done

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Loading = () => {
-  return <h1>Loading</h1>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>
+        <b>Loading...</b>
+      </h1>
+      ;
+    </div>
+  );
 };
 
 export default Loading;
